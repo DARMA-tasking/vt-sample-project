03bd98621b7d8866c3bb72968ec1f615bd4905a9
Jonathan Lifflander <jliffla@sandia.gov>
2023-03-28 14:53:04 -0700

    #13: build: remove detector

--- build_with_tpl.cmake
+++ build_with_tpl.cmake
@@ -2,15 +2,12 @@ message(STATUS "Building with TPL")
 
 set(vt_DIR "" CACHE STRING "Path to VT")
 set(checkpoint_DIR "" CACHE STRING "Path to checkpoint")
-set(detector_DIR "" CACHE STRING "Path to detector")
 
 # Create symbolic links for adding as a subdirectory to test with all DARMA
 # libraries at the same level
 execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${vt_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/TPL/vt)
 execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${checkpoint_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/TPL/checkpoint)
-execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${detector_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/TPL/detector)
 
-add_subdirectory(TPL/detector)
 add_subdirectory(TPL/checkpoint)
 add_subdirectory(TPL/vt)
 
@@ -23,7 +20,6 @@ add_executable(
   "${CMAKE_SOURCE_DIR}/vt-runtime/src/sample.cc"
 )
 
-target_link_libraries(${RUNTIME_SAMPLE} PUBLIC detector)
 target_link_libraries(${RUNTIME_SAMPLE} PUBLIC checkpoint)
 target_link_libraries(${RUNTIME_SAMPLE} PUBLIC vt)
 ############################################################
@@ -36,7 +32,6 @@ add_executable(
   "${CMAKE_SOURCE_DIR}/vt-trace-only/src/sample.cc"
 )
 
-target_link_libraries(${TRACE_ONLY_SAMPLE} PUBLIC detector)
 target_link_libraries(${TRACE_ONLY_SAMPLE} PUBLIC checkpoint)
 target_link_libraries(${TRACE_ONLY_SAMPLE} PUBLIC vt-trace)
 ############################################################
